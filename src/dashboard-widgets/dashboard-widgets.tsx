9bba7347d5a0f25ffe9a0718186f4e36ab91b813
Osoro, Chester <Chester.Osoro@thepalladiumgroup.com>
2019-06-20 11:35:12 +0300

    import diagnosis parcel as a widget

--- src/dashboard-widgets/dashboard-widgets.tsx
+++ src/dashboard-widgets/dashboard-widgets.tsx
@@ -3,43 +3,45 @@ import { css } from "@emotion/core";
 import Parcel from "single-spa-react/parcel";
 
 const patientDashboardParcels = [
-  // A patient dashboard widget within this git repo
-  () => import("./basic-info/basic-info.parcel").then(m => m.default),
+    // A patient dashboard widget within this git repo
+    () => import("./basic-info/basic-info.parcel").then(m => m.default),
+    () => System.import("@hackathon/diagnosis-widget")
+
 ];
 
 export default function DashboardWidgets(props: DashboardWidgetsProps) {
-  return (
-    <div
-      css={css`
+    return (
+        <div
+            css={css`
         margin: 76px auto 0 auto;
         width: 500px;
       `}
-    >
-      {patientDashboardParcels.map((parcel, i) => (
-        <div
-          css={css`
+        >
+            {patientDashboardParcels.map((parcel, i) => (
+                <div
+                    css={css`
             background-color: white;
             border-radius: 5px;
             box-shadow: 0 10px 30px -24px #b3b3b3;
             padding: 16px;
             margin-bottom: 16px;
           `}
-          key={i}
-        >
-          <Parcel
-            config={parcel}
-            patientUuid={props.match.params.patientUuid}
-          />
+                    key={i}
+                >
+                    <Parcel
+                        config={parcel}
+                        patientUuid={props.match.params.patientUuid}
+                    />
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 type DashboardWidgetsProps = {
-  match: {
-    params: {
-      patientUuid: string,
+    match: {
+        params: {
+            patientUuid: string,
+        }
     }
-  }
 };
